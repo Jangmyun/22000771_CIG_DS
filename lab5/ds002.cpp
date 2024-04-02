15a861b887bd3c3110c3fcb136d2f86727a22988
Jangmyun <cndlsrb2739@gmail.com>
2024-04-02 17:11:07 +0900

    좌표간 거리계산 프로그램 완성

---
+++ lab5/ds002.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+  double x1, y1;
+  double x2, y2;
+
+  scanf("%lf %lf", &x1, &y1);
+  scanf("%lf %lf", &x2, &y2);
+
+  double result = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+  printf("%.1lf\n", result);
+}

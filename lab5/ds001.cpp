c1496edce8ffd8c905ba5f31f495281fe4b55bb5
Jangmyun <cndlsrb2739@gmail.com>
2024-04-02 16:54:16 +0900

    ds001 완료

--- lab5/ds001.cpp
+++ lab5/ds001.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <cstdio>
+
 using namespace std;
 
 int main()
 {
+  int height, weight;
+  double bmi;
+  scanf("%d %d", &height, &weight);
+  double heightM = (double)height / 100;
+  bmi = weight / (heightM * heightM);
+  cout << bmi << endl;
+
+  if (bmi >= 25)
+  {
+    cout << "Yes" << endl;
+  }
+  else
+  {
+    cout << "No" << endl;
+  }
 }

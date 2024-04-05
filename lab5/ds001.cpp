5b26c90f6d9e2562540fa80f4c738d6d6016c843
Jangmyun <cndlsrb2739@gmail.com>
2024-04-05 21:57:11 +0900

    주석처리

--- lab5/ds001.cpp
+++ lab5/ds001.cpp
@@ -7,11 +7,12 @@ int main()
 {
   int height, weight;
   double bmi;
-  scanf("%d %d", &height, &weight);
-  double heightM = (double)height / 100;
-  bmi = weight / (heightM * heightM);
 
-  if (bmi >= 25)
+  scanf("%d %d", &height, &weight);
+  double heightM = (double)height / 100; // m단위로 변환
+  bmi = weight / (heightM * heightM);    // bmi 연산
+
+  if (bmi >= 25) // bmi 25 이상이면 yes, 아니면 no 출력
   {
     cout << "Yes" << endl;
   }

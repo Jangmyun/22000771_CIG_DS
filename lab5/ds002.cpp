5ec838ab78f13f1d3f39b9bf200da54ab8a6a5ca
Jangmyun <cndlsrb2739@gmail.com>
2024-04-05 22:10:53 +0900

    주석 처리

--- lab5/ds002.cpp
+++ lab5/ds002.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdio>
-#include <cmath>
+#include <cmath> // cmath 라이브러리
 using namespace std;
 
 int main()
@@ -11,9 +11,9 @@ int main()
   scanf("%lf %lf", &x1, &y1);
   scanf("%lf %lf", &x2, &y2);
 
-  double result = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+  double result = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)); // 두 점의 거리  = (x좌표 차이의 제곱 + y좌표 차이의 제곱 ) 의 루트
 
-  printf("%.1lf\n", result);
+  printf("%.1lf\n", result); // 소수점 1자리 까지 프린트
 
   return 0;
 }

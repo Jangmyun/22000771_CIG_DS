1e0cf64cb4a09849e878a2da564a3da6d0c641cd
Jangmyun <cndlsrb2739@gmail.com>
2024-04-06 00:47:47 +0900

    주석 처리

--- lab5/ds005.cpp
+++ lab5/ds005.cpp
@@ -6,21 +6,21 @@ string toUpperCase(string str);
 int main()
 {
   string input;
-  int alphaNum[27] = {0};
+  int alphaNum[27] = {0}; // 공백 + 알파벳  = 27개의 int 배열을 0으로 초기화
 
-  getline(cin, input);
-  input = toUpperCase(input);
+  getline(cin, input);        // 한줄 입력받기
+  input = toUpperCase(input); // 대문자로 바꾸기
   cout << input[3] << endl;
 
-  for (int i = 0; i < input.size(); i++)
+  for (int i = 0; i < input.size(); i++) // 입력받은 문자열의 사이즈만큼 반복
   {
-    if (input[i] == ' ')
+    if (input[i] == ' ') // 공백일 경우 0번째 원소 1추가
     {
       alphaNum[0] += 1;
     }
-    else
+    else //
     {
-      int index = (int)input[i] - 64;
+      int index = (int)input[i] - 64; // 알파벳(대문자) 의 경우 A가 아스키 65부터 시작 & alphaNum 배열 1부터 시작  ==> char을 int로 형변환하고 64를 빼면 배열의 인덱스가 됨
       alphaNum[index] += 1;
     }
   }
@@ -45,7 +45,7 @@ int main()
   return 0;
 }
 
-string toUpperCase(string str)
+string toUpperCase(string str) // 문자열을 대문자로 변환
 {
   string output;
   for (int i = 0; i < str.size(); i++)

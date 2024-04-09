a7c308858608088ac47acf329c5aaca8a92f222c
Jangmyun <cndlsrb2739@gmail.com>
2024-04-09 16:39:57 +0900

    ds011

--- lab6/ds011.cpp
+++ lab6/ds011.cpp
@@ -4,9 +4,25 @@ using namespace std;
 int main()
 {
   int N;
+  int max = 0;
+  int sum = 0;
+
   cin >> N;
 
-  int *arr = new int;
+  int *arr = new int[N];
 
+  for (int i = 0; i < N; i++)
+  {
+    cin >> arr[i];
+    sum += arr[i];
+    if (max < arr[i])
+    {
+      max = arr[i];
+    }
+  }
+
+  cout << sum << " " << max << endl;
+
+  delete[] arr;
   return 0;
 }

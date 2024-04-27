9cb78e097fcb9b8b003bb18eac80844e54555cb6
Jangmyun <cndlsrb2739@gmail.com>
2024-04-27 16:31:46 +0900

    주석 처리

--- lab7/ds021.cpp
+++ lab7/ds021.cpp
@@ -3,25 +3,26 @@ using namespace std;
 
 class Student {
  private:
-  double getAvg() const;
+  double getAvg()
+      const;  // const => 객체 내부 변수 변경 불가 & const 함수만 호출 가능
 
  public:
   string name;
   string sid;
-  int *score;
+  int *score;  // 국영수 점수 받을 포인터 멤버 변수
   void print() const;
 };
 
 void Student::print() const {
-  cout << '[' << sid << "] " << name << endl;
-  cout << fixed;
+  cout << "[" << sid << "] " << name << endl;
+  cout << fixed;  // 소수점 한자리 제한해서 출력
   cout.precision(1);
   cout << "The Average score is " << getAvg() << endl;
 }
 double Student::getAvg() const {
   double avg;
   int sum = 0;
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 3; i++) {  // score 포인터 멤버변수를 배열처럼 사용
     sum += score[i];
   }
   avg = (double)sum / 3;

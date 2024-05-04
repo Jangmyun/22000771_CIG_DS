1e4f4d93725361d65d4ed97b86a9f142c33afdf6
Jangmyun <cndlsrb2739@gmail.com>
2024-05-04 14:19:13 +0900

    주석처리

--- lab9/ds031.cpp
+++ lab9/ds031.cpp
@@ -40,24 +40,25 @@ class MyStack {
 };
 
 void start() {
-  int N;
+  int N;  // 스택 list 크기 입력받기
   cin >> N;
-  MyStack s(N);
+  MyStack s(N);  // 입력받은 크기의 list를 가진 스택 s 생성
 
-  string in;
-  int value;
-  while (in != "print") {
+  string in;               // 명령어 입력받을 변수
+  int value;               // 명령어 뒤에 입력될 값
+  while (in != "print") {  // 명령어 print가 나올 때 까지 반복
     cin >> in;
+    // 명령어가 push면 뒤에 숫자도 읽어서 Element에 값을 넣어 push
     if (in == "push") {
       cin >> value;
       Element t;
       t.value = value;
       s.push(t);
     }
-    if (in == "pop") {
+    if (in == "pop") {  // pop 이면 뒤 값 읽지 않고 스택 pop
       s.pop();
     }
-    if (in == "print") {
+    if (in == "print") {  // print면 반복문 종료 후 print() 실행
       break;
     }
   }

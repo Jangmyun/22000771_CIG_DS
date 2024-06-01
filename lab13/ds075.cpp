d056430d3a2d102c93192467c65a7617ad115d87
Jangmyun <cndlsrb2739@gmail.com>
2024-06-01 22:57:25 +0900

    print함수 추가 / 제거

--- lab13/ds075.cpp
+++ lab13/ds075.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 using namespace std;
+
 #include "./lib/sorting.h"
 
 int main() {
   int n;
   cin >> n;
   int arr[n];
+  int *output;
 
   for (int i = 0; i < n; i++) {
     cin >> arr[i];
   }
 
-  selectionSort(arr, n);
-  print(arr, n);
+  mergeSort(arr, n);
   return 0;
 }

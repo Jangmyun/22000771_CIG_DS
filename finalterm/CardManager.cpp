f40a7a03c6d0da3c0a8fc43ee241b6619d3cac3e
Jangmyun <cndlsrb2739@gmail.com>
2024-06-18 14:54:01 +0900

    .

--- finalterm/CardManager.cpp
+++ finalterm/CardManager.cpp
@@ -1,83 +1,105 @@
 #include "cardManager.h"
-#include <iostream>
+
 #include <fstream>
+#include <iostream>
+#include <map>
 #include <sstream>
 #include <vector>
-#include <map>
 
 extern map<string, Card*> allclasses;
+extern map<string, double> gradescore;
 
-CardManager::~CardManager(){
-    vector<Card*>::iterator iter;
-    for (iter = myclasses.begin(); iter != myclasses.end(); ++iter) {
-		delete (*iter);
-	}
+CardManager::~CardManager() {
+  vector<Card*>::iterator iter;
+  for (iter = myclasses.begin(); iter != myclasses.end(); ++iter) {
+    delete (*iter);
+  }
 }
 
-void CardManager::printAll(){
-    for (int i=0; i<myclasses.size(); i++){
-        cout << i+1 << " | " << myclasses[i]->toString() << endl;
+void CardManager::printAll() {
+  int i;
+  double total = 0;
+  int credit = 0;
+  double GPA = 0.0;
+  double gs;  // grade score
+
+  for (i = 0; i < myclasses.size(); i++) {
+    gs = gradescore.find(myclasses[i]->getCredit());
+    cout << i + 1 << " | " << myclasses[i]->toString() << endl;
+  }
+}
+
+void CardManager::addCard() {
+  string code, grade;
+  cout << ">> Enter class code > ";
+  cin >> code;
+
+  for (vector<Card*>::size_type i = 0; i < myclasses.size(); i++) {
+    if (myclasses[i]->getCode() == code) {
+      cout << "The Card of " << code << " already exists." << endl;
+      return;
     }
-}
+  }
 
-void CardManager::addCard(){
-    string code, grade;
-	cout << ">> Enter class code > ";
-	cin >> code;
-    if(allclasses.count(code)>0){
-        cout << allclasses[code]->toStringShort() << endl;
-	    cout << ">> Enter grade > ";
-	    cin >> grade;
-        Card* new_card = new Card(allclasses[code], grade);
-        myclasses.push_back(new_card);
-        count++;
-        total_credit += new_card->getCredit();
+  if (allclasses.count(code) > 0) {
+    cout << allclasses[code]->toStringShort() << endl;
+    cout << ">> Enter grade > ";
+    cin >> grade;
+
+    if (gradescore.find(grade) == gradescore.end()) {
+      cout << "Wrong grade!" << endl;
+      return;
     }
-    else{
-        cout << "No such class." << endl;
+
+    Card* new_card = new Card(allclasses[code], grade);
+    myclasses.push_back(new_card);
+    count++;
+    total_credit += new_card->getCredit();
+  } else {
+    cout << "No such class." << endl;
+  }
+}
+
+void CardManager::editCard() {
+  int no;
+  string new_grade;
+  cout << ">> Enter a Card number > ";
+  cin >> no;
+  cout << myclasses[no - 1]->toString() << endl;
+  cout << ">> Enter new grade > ";
+  cin >> new_grade;
+  myclasses[no - 1]->setGrade(new_grade);
+  cout << "Grade changed." << endl;
+}
+
+void CardManager::sortByCode() {
+  sort(myclasses.begin(), myclasses.end(), Card::cmpCode);
+}
+
+void CardManager::loadCards(string filename) {
+  string line, code, grade;
+  int count = 0;
+  ifstream file(filename);
+  while (!file.eof()) {
+    if (file.eof()) break;
+    file >> code >> grade;
+    Card* new_card = new Card(allclasses[code], grade);
+    myclasses.push_back(new_card);
+    count++;
+    total_credit += new_card->getCredit();
+  }
+  file.close();
+  cout << count << " classes are loaded.\n";
+}
+
+void CardManager::findCards(string name) {
+  int found = 0;
+  cout << "Searching keyword: " << name << endl;
+  for (int i = 0; i < myclasses.size(); i++) {
+    if (myclasses[i]->getName().find(name) != string::npos) {
+      found++;
+      cout << i + 1 << " | " << myclasses[i]->toString() << endl;
     }
-}
-
-void CardManager::editCard(){
-	int no;
-    string new_grade;
-	cout << ">> Enter a Card number > ";
-	cin >> no;
-    cout << myclasses[no-1]->toString() << endl;
-    cout << ">> Enter new grade > ";
-    cin >> new_grade;
-    myclasses[no-1]->setGrade(new_grade);
-    cout << "Grade changed." << endl;	
-}
-
-void CardManager::sortByCode(){
-    sort(myclasses.begin(), myclasses.end(), Card::cmpCode);
-}
-
-void CardManager::loadCards(string filename){
-    string line, code, grade;
-    int count=0;
-	ifstream file(filename);
-	while(!file.eof()){
-        if(file.eof()) break;
-		file >> code >> grade;
-        Card* new_card = new Card(allclasses[code], grade);
-        myclasses.push_back(new_card);
-        count++;
-        total_credit += new_card->getCredit();
-	}
-	file.close();
-    cout << count << " classes are loaded.\n";
-}
-
-void CardManager::findCards(string name){
-	int found = 0;
-	cout << "Searching keyword: " << name << endl;
-    for (int i=0; i<myclasses.size(); i++){
-		if(myclasses[i]->getName().find(name) != string::npos){
-			found++;
-            cout << i+1 << " | " << myclasses[i]->toString() << endl;
-		}
-	}
-	cout << count << " classes found.\n";
+  }
+  cout << count << " classes found.\n";
 }
--- finalterm/card.h
+++ finalterm/card.h
@@ -4,28 +4,28 @@
 #include <iostream>
 using namespace std;
 
-class Card{
-private:
-    string code;
-    string name;
-    int credit;
-    string grade;
-    double score;
-    
-public:
-    Card(string code, string name, int credit);
-    Card(string code, string name, int credit, string grade);
-    Card(Card* card, string grade);
-    ~Card(){};
-    void setGrade(string newgrade){grade = newgrade;}
-    string getCode(){return code;};
-    string getName(){return name;};
-    int getCredit(){return credit;};
-    string toString();
-    string toStringShort();
+class Card {
+ private:
+  string code;
+  string name;
+  int credit;
+  string grade;
+  double score;
 
-    static bool cmpCode(Card* c1, Card* c2){
-        return (c1->getCode() < c2->getCode()); 
-    }
+ public:
+  Card(string code, string name, int credit);
+  Card(string code, string name, int credit, string grade);
+  Card(Card* card, string grade);
+  ~Card(){};
+  void setGrade(string newgrade) { grade = newgrade; }
+  string getCode() { return code; };
+  string getName() { return name; };
+  int getCredit() { return credit; };
+  string toString();
+  string toStringShort();
+
+  static bool cmpCode(Card* c1, Card* c2) {
+    return (c1->getCode() < c2->getCode());
+  }
 };
 #endif
